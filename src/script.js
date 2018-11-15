c1ec1fee0e4e99d23e0bd75264717623966fd67d
17Octopus17 <kuznetsova.olga17@gmail.com>
2018-11-15 17:49:09 +0200

    tab

---
+++ src/script.js
@@ -0,0 +1,18 @@
+
+(function () {
+  const x = document.querySelectorAll('.tablinks');
+  for (let i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const y = document.querySelectorAll('[data-content]');
+      for (let j = 0; j < y.length; j++) {
+        if (y[j].dataset.content === x[i].dataset.button) {
+          y[j].classList.add('display');
+        } else {
+          y[j].classList.remove('display');
+        }
+      }
+    });
+  }
+}());
